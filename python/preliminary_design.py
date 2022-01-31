565a79c33991457877e845be477116268b4cde22
f-grimm <f.grimm@tum.de>
2022-01-31 16:12:40 +0100

    Update preliminary_design.py

--- python/preliminary_design.py
+++ python/preliminary_design.py
@@ -8,14 +8,21 @@ Created on Tue Jan 18 19:36:45 2022
 from modules.aircraft import *
 from modules.rotor import *
 
+"""
+"""
 
-concept = Aircraft()
-concept.main_rotor = Rotor()
+# Create aircraft object with initial values from YAML file
+aircraft = Aircraft('concept_01')
 
-concept.mtow = concept.get_initial_mtow()
-print("First MTOW estimation: {:7.1f} kg".format(concept.mtow))
+# Add main rotor
+aircraft.main_rotor = Rotor('concept_01')
 
-concept.iterate_first_sizing()
-print('Hover power: {:17.1f} kW'.format(concept.hover_power / 1000))
+# Estimate MTOW based on mission profile
+aircraft.mtow = aircraft.get_initial_mtow()
+print("First MTOW estimation: {:7.1f} kg".format(aircraft.mtow))
+
+# Perform one step in the first sizing loop
+aircraft.iterate_first_sizing()
+print('Hover power: {:17.1f} kW'.format(aircraft.hover_power / 1000))
 
 
