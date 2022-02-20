5637ef8995cc5aeaf37ce6809f2a1a2277bc6ef2
f-grimm <f.grimm@tum.de>
2022-02-20 19:42:27 +0100

    Update for separate mission file

--- python/_main.py
+++ python/_main.py
@@ -12,13 +12,13 @@ from preliminary_design import preliminary_design, plot_powers
 """
 
 # Create aircraft with initial values from YAML file
-aircraft = Aircraft('concept_01.yaml')
+aircraft = Aircraft('concept_01', mission='hc')
 
 # Start premilinary design
-preliminary_design(aircraft, logs=True)
+preliminary_design(aircraft, mission_segment=1, logs=True)
 
 # Plot mission profile
-aircraft.mission.plot_mission()
+# aircraft.mission.plot_mission()
 
 # Plot power over flight speed
-plot_powers(aircraft, max_velocity=80)
+# plot_powers(aircraft, max_velocity=80)
